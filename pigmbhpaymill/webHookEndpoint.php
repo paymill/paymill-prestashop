bdae44b379f453c7fb82eef32370cf8e5b7b1e81
Christopher <Christopher@Christopher-PC.Speedport_W_723V_1_27_000>
2014-05-26 17:24:57 +0200

    PMPS-67 refactor webHookEndpoint

--- pigmbhpaymill/webHookEndpoint.php
+++ pigmbhpaymill/webHookEndpoint.php
@@ -6,10 +6,10 @@ include_once(dirname(__FILE__) . '/pigmbhpaymill.php');
 $request = json_decode(@file_get_contents('php://input'), true);
 
 if (validateNotification($request)) {
-    $orderId = getOrderIdFromNotification($request['event']['event_resource']['transaction']['description']);
+    $order_id = getOrderIdFromNotification($request['event']['event_resource']['transaction']['description']);
     $paymill = new PigmbhPaymill();
-    $paymill->updateOrderState($orderId);
-    echo "OK";
+    $paymill->updateOrderState($order_id);
+    echo 'OK';
 }
 
 
@@ -21,15 +21,21 @@ function validateNotification($notification)
         if (isset($notification['event']['event_type'])) {
             if ($notification['event']['event_type'] == 'refund.succeeded') {
                 $id = null;
-                if (isset($notification['event']['event_resource']['transaction']['id'])) {
+                if (isset($notification['event']['event_resource']['transaction']['id']))
                     $id = $notification['event']['event_resource']['transaction']['id'];
-                }
-                $transactionObject = new Services_Paymill_Transactions(Configuration::get('PIGMBH_PAYMILL_PRIVATEKEY'), 'https://api.paymill.com/v2/');
-                $result = $transactionObject->getOne($id);
+                
+                
+                $transaction_object = new Services_Paymill_Transactions(
+                    Configuration::get('PIGMBH_PAYMILL_PRIVATEKEY'), 
+                    'https://api.paymill.com/v2/'
+                );
+                
+                $result = $transaction_object->getOne($id);
                 return $result['id'] === $id;
             }
         }
     }
+    
     return false;
 }
 
@@ -37,8 +43,8 @@ function getOrderIdFromNotification($transactionDescription)
 {
     $regexPattern = '/OrderID: (\d+)/i';
     $matches = array();
-    if (preg_match($regexPattern, $transactionDescription, $matches)) {
+    if (preg_match($regexPattern, $transactionDescription, $matches))
         return (int)$matches[1];
-    }
+    
     return false;
 }
