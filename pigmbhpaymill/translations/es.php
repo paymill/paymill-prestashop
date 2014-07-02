61c5d8a0595573c9d0f277872096c208b2ad3705
Stefan Harenkamp <stefan.harenkamp@payintelligent.de>
2014-07-02 18:18:01 +0200

    added snippets for es

--- pigmbhpaymill/translations/es.php
+++ pigmbhpaymill/translations/es.php
@@ -1,79 +1,31 @@
 <?php
-/**
-* 2012-2014 PAYMILL
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-*
-*  @author    PAYMILL <support@paymill.com>
-*  @copyright 2012-2014 PAYMILL
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*/
 
+global $_MODULE;
 $_MODULE = array();
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_1ef3e269a9500231415bd231bb29ecf6"] = "Pagos con tarjeta de crédito PAYMILL y con dedomiciliación bancaria";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_8e40c7d5748c55e0f5f034dd566493de"] = "Ha ocurrido un error mientras procesábamos tu pago.";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_14a327b10605f16f336e285a50fc471b"] = "Mostrar detalles";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_565a30e7a71104fee1947d5a61ce05df"] = "Configuración del pago";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_25640062516acc81cd2220f3f02ee6a5"] = "Activar tarjeta de crédito";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_16ac6c100d081e5728ea9c9acf823fed"] = "Activar débito directo";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_d4369c99462fe1a027560deecf756853"] = "Configuración principal";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_d2560860c51f895a9871741f0805c39e"] = "Clave privada";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_37c5b6e7c4291021b6100a6754ae7ffe"] = "Clave pública";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_4355c4db961539628702c5818ddb71df"] = "Activar debugging";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_3132e398e6e7acace9290305a55a7d25"] = "Activar el registro";
-$_MODULE['<{pigmbhpaymill}prestashop>pigmbhpaymill_59a10f6da66d0a5a4f7d7c76fa275b01'] = 'Akzeptierte Kreditkarten';
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_db5acc95be6e6b2a02e5bf3516387883"] = "Activar la compra rápida";
-$_MODULE["<{pigmbhpaymill}prestashop>pigmbhpaymill_c9cc8cce247e49bae79f15173ce97354"] = "Guardar";
+$_MODULE['<{pigmbhpaymill}prestashop>pigmbhpaymill_10c5644846787c7bedd95a27ac46649a'] = 'Pagos sencillos a través de Internet. Instálelo ahora y pruébelo gratis sin compromiso.';
+$_MODULE['<{pigmbhpaymill}prestashop>pigmbhpaymill_8e40c7d5748c55e0f5f034dd566493de'] = 'Ha ocurrido un error mientras procesábamos tu pago.';
+$_MODULE['<{pigmbhpaymill}prestashop>pigmbhpaymill_14a327b10605f16f336e285a50fc471b'] = 'Mostrar detalles';
 $_MODULE['<{pigmbhpaymill}prestashop>validation_56a51b7f616a536703192e16850d23c2'] = 'ELV / SEPA Days until the debit: ';
 $_MODULE['<{pigmbhpaymill}prestashop>validation_73a61696f100b3858511e212a3feea6b'] = 'Credit Card';
-$_MODULE["<{pigmbhpaymill}prestashop>logging_6c7794632edc21ff452a3abed8adb2f8"] = "Búsqueda conectada";
-$_MODULE["<{pigmbhpaymill}prestashop>logging_dcafb98d01cdeea2ff573f2a06c48b58"] = "Buscar";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_295f491189901501cb93ddd02b0a2250"] = "CVC inválido";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_196a5aa05f160321a32eef428d2cea89"] = "Por favor, introduce el nombre del titular de la tarjeta";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_773fcd7a2070fcf96c72ea6540c77eb8"] = "Fecha de expiración inválida";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_b6aea06643321d8ca987415b272529a5"] = "Por favor, introduce un número válido de tarjeta de crédito.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_02c1ceb76211fab1438280f0e7566163"] = "Por favor, introduce el nombre del titular de la cuenta para la domiciliación bancaria.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_60f59fa92ef774c868b7f6c3809faae3"] = "Por favor, introduce un número válido de la cuenta para la domiciliación bancaria.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_d9c3a431f3d766dd8e73bae03e1fca73"] = "Por favor, introduce un código bancario válido de domiciliación bancaria.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_a819b1df441ad6e36a1ee7090e31a617"] = "Please enter your iban.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_2b750d494a62d5cd48c997a1cf8c1c44"] = "Please enter your bic.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_b3ea5d4962bbcb423b6e3693d7a1ea9f"] = "Unknown Error";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_3a1352bdd435ae1a6bca9f1a905356d4"] = "The communication with the psp failed.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_3b227bb4f8950c16f67421a55079986b"] = "The public key is invalid.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_f08cca2968ba7813ffa44fff9e221902"] = "Paymentmethod, card type currency or country not authorized";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_503a2cbd129fd8ab8774f621b306b328"] = "3-D Secure process has been canceled by the user";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_77cd46455e8ee7891f926b8e8bb9df43"] = "Invalid Credit Card Number";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_f8e076a11e165d0e144f867a1bbeed66"] = "Credit Card not valid";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_074cb1b595ba0e584a5f92b8293d64e0"] = "Invalid CVC";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_4fa2239b0db3dd54dccb3a5d276d6ae7"] = "Invalid Card Holder";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_28fd10d18fb882dadf2eb2efe59f7b5b"] = "Missing amount for 3-D Secure";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_27cdc21f082b376b1bbf9a8bba5d3de4"] = "Invalid currency for 3-D Secure";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_dc220ffa95c3b950a1acac241f164373"] = "Invalid Account Number";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_057e6576deeeaa807f87002fa2ad11c6"] = "Invalid Account Holder";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_c27555a8a125335f38a1f5ce0f6b8015"] = "Invalid bank code";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_c3df34d3f1b7afde626032d07efd8dbf"] = "Invalid IBAN";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_5d634cf37a5af374207cbbb5839f9da1"] = "Invalid BIC";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_9663f7ffe12ce54f0e5f1d88e88870eb"] = "Invalid country for sepa transactions";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_f20244939fb834096e7a3a5373cb124e"] = "Invalid bank data";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_f1d3b424cd68795ecaa552883759aceb"] = "Revisión del pedido";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_572da7ef1411f2a12409e752f3eb2f7a"] = "Su cesta de la compra está vacía";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_3448ea8312665651913759d18a33a766"] = "Número de tarjeta";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_b2844b8e17ecaaeae68d018fe9418af0"] = "Válida hasta";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_459637c303466a9edd49d831d0b70c9e"] = "Titular de la tarjeta";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_5ea7f231296949070013bc43715aae01"] = "Código CVC";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_1c456c8aa0e37e68a9caabc09b39be9f"] = "El código CVV o CVC es una medida de seguridad de las tarjetas de crédito. Normalmente es un número de tres a cuatro dígitos de longitud. En las tarjetas de crédito VISA, se le llama código CVV. Se puede encontrar este mismo código en las tarjetas de crédito MasterCard -donde se le llama CVC. CVC es la abreviatura de \"código de validez de la tarjeta\". El código CVV, por otro lado, es la abreviatura de \"código de valor de verificación de la tarjeta\". Parecidas a MasterCard y Visa, otras tarjetas como Diners Club, Discover y JCB contienen un número de tres dígitos que se encuentra normalmente en el reverso de la tarjeta de crédito. Las tarjetas MAESTRO pueden tener o no el código CVV de tres dígitos. En caso de usar una tarjeta MAESTRO sin CVV, puede introducir 000 en el formulario en su lugar. American Express usa el CID (número de identificación de la tarjeta). El CID es un número de cuatro dígitos que normalmente se encuentra en el anverso de la tarjeta, arriba a la derecha del número de la tarjeta de crédito.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_0e7c2b864af10317bf7ca889a31f863f"] = "Número de cuenta";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_fbc4693ad8cd8fd628848fcfc2378f13"] = "Código bancario";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_82130ba6c49d07bc4ebda5a45a871b90"] = "Titular de la cuenta";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_db4d7b1139c5f5887821a54d8ef27c71"] = "Cantidad";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_9fa45a4dfd768b1faa4d245792fbd185"] = "Los campos marcados con un * son obligatorios.";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_3adf712b8413a09c3c9de40a5b23fc5d"] = "Selección del pago";
-$_MODULE["<{pigmbhpaymill}prestashop>paymentform_a240fa27925a635b08dc28c9e4f9216d"] = "Pedido";
-$_MODULE["<{pigmbhpaymill}prestashop>confirmation_7a223127bd5f4a87a989fb8c560bb15b"] = "Gracias por su pedido";
-$_MODULE["<{pigmbhpaymill}prestashop>payment_9300f4d366a0f5e0b57afef7e8a7474d"] = "Tarjeta de crédito";
-$_MODULE["<{pigmbhpaymill}prestashop>payment_89ce55b91d58a25f4271514be6b1146e"] = "Domiciliación bancaria";
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_b797d7be96efe55022d380168de71341'] = 'PAYMILL le ofrece un servicio de pagos integral. Brinde a sus clientes la oportunidad de pagar con tarjeta de crédito o a través de domiciliación bancaria directamente en su página web, sin necesidad de redirigirlos a otras páginas. Benefíciese de mayores tasas de compra.';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_6f0d1a800a97d64f6ded9d34ed2218ac'] = 'Seguridad garantizada';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_ad198f28b49508621630f8e96895abc8'] = 'PAYMILL cumple con todos los requisitos del nivel 1 de PCI, así como con toda la normativa europea y de industria sobre protección de datos. Con certificación y aprobación independiente de TÜV Saarland. Los datos de los clientes son tramitados por PAYMILL, por lo que Vd. podrá ofrecer pagos online de forma rápida y sencilla sin necesidad de contar con una certificación propia.';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_476d74821c0e25a8492876f2306aadc9'] = 'Pagos online en todo el mundo';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_c628cfff16277a01f239c37422782780'] = 'Gánese la confianza de sus clientes y aumente su capacidad de negocio ofreciendo la posibilidad de pagar hasta en 100 divisas diferentes en todo el mundo.';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_f78b5536279ec678b96a2abbf6207563'] = 'Compras con un solo clic ';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_8355a48fe0d6e7c66d69fae07c5390c3'] = 'Aumente la comodidad de sus clientes: con PAYMILL, los clientes podrán guardar sus datos para próximas compras, de modo que baste un clic para confirmar.';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_b1fa6b4767532c2e890022101221bd08'] = 'Sus ventajas';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_ca81d1a2d3927e7df1f900a9d0da4bf3'] = 'Se aceptan todas las tarjetas: Visa, MasterCard, American Express, etc.';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_705c141fe40c7d369fbe881cc4a7584f'] = 'Servicio disponible para proveedores en 40 países';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_de4b913eed1a6462d96bd6139cce1704'] = 'Apertura gratuita de una cuenta de proveedor';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_dffeb6e510306bce0d957ac1eea70e4d'] = 'Numerosas alternativas para prevención de fraudes';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_c82a4d8d37c128f08573333b4b0f76fe'] = 'Sin tasas de alta ni costes mensuales fijos';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_b6c5af640d4dd5c3d0ee01c2cd82734e'] = 'Pago exclusivamente por transacción realizada; p.ej. 2,95% + 0,28 € (gastos bancarios incluidos)';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_f29d17ad6c00ba3b56a7dd15fe474144'] = 'Fácil instalación y configuración';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_85d64b010adfc15b11dac81ebd47d7cc'] = 'Compras sin redireccionamientos ni marcos incorporados (iFrames)';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_24e27204567402851da65f2b97b66fbd'] = 'Se aceptan hasta 100 divisas diferentes';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_c6b3373b70458e7f98e6a0c0db876ce1'] = 'Abra ahora su cuenta gratuita de PAYMILL y reciba las claves de prueba (Test Keys).';
+$_MODULE['<{pigmbhpaymill}prestashop>paymillheader_0ef6c30ad9be63a09cecd264fbc13d77'] = 'ABRIR CUENTA';
+$_MODULE['<{pigmbhpaymill}prestashop>confirmation_7a223127bd5f4a87a989fb8c560bb15b'] = 'Gracias por su pedido';
+$_MODULE['<{pigmbhpaymill}prestashop>payment_9300f4d366a0f5e0b57afef7e8a7474d'] = 'Tarjeta de crédito';
+$_MODULE['<{pigmbhpaymill}prestashop>payment_89ce55b91d58a25f4271514be6b1146e'] = 'Domiciliación bancaria';
