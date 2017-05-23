fcf26b53a6eb918fed7ab5e595b79c1528c6a7ea
Tschela Baumann <baumann@itopia.de>
2017-05-23 11:35:17 +0200

    Remove certificate authority info

--- paymill/v2/lib/Services/Paymill/Apiclient/Curl.php
+++ paymill/v2/lib/Services/Paymill/Apiclient/Curl.php
@@ -47,7 +47,7 @@ class Services_Paymill_Apiclient_Curl implements Services_Paymill_Apiclient_Inte
      */
     private $_apiUrl = '/';
 
-    const USER_AGENT = 'Paymill-php/0.0.2';
+    const USER_AGENT = 'Paymill-Prestashop/2.1.1';
 
     public static $lastRawResponse;
     public static $lastRawCurlOptions;
@@ -123,8 +123,7 @@ class Services_Paymill_Apiclient_Curl implements Services_Paymill_Apiclient_Inte
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_CUSTOMREQUEST => $method,
             CURLOPT_USERAGENT => self::USER_AGENT,
-            CURLOPT_SSL_VERIFYPEER => true,
-            CURLOPT_CAINFO => realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'paymill.crt',
+            CURLOPT_SSL_VERIFYPEER => true
         );
 
         if (Services_Paymill_Apiclient_Interface::HTTP_GET === $method) {
